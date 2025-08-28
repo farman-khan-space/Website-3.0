53e3378ff2fb3e4b18f29f9af85df099d3fbe2f6
ray-red19 <crwirteron@gmail.com>
2025-08-28 22:20:50 +0500

    Add logo to header

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("img"); // <--- I added this line
 
   return {
     dir: {
