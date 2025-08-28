002c93618fd839cf502ccda69c175149160265ed
ray-red19 <crwirteron@gmail.com>
2025-08-28 14:32:01 +0500

    Fix eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "pages",
-      includes: "/_includes",
+      includes: "../_includes",  // go up one level from pages/
       output: "_site"
     },
     htmlTemplateEngine: "njk",
