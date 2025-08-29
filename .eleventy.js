a8eb7cb6b27dacadbc598fd2086d234f5dbb8b9d
ray-red19 <crwirteron@gmail.com>
2025-08-29 14:25:19 +0500

    Add mechanisim for .md posts and projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("img"); // <--- I added this line
+  eleventyConfig.addPassthroughCopy("img");
+
+  // Add this line to create the 'limit' filter
+  eleventyConfig.addFilter("limit", (arr, limit) => (arr || []).slice(0, limit));
 
   return {
     dir: {
       input: "pages",
-      includes: "../_includes",  // go up one level from pages/
+      includes: "../_includes",
       output: "_site"
     },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     pathPrefix: process.env.ELEVENTY_ENV === "production" 
       ? "/Website-3.0/" 
-      : "/"   // keep localhost clean
+      : "/"
   };
 };
